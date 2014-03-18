40516fe899d8a39217bb5ed3faad5b73448c520c
jtimon <jtimon@monetize.io>
2014-03-18 16:33:19 -0700

    Add get_version function

--- zmqmin/__init__.py
+++ zmqmin/__init__.py
@@ -1,6 +1,12 @@
 
 from zmq import Context, ZMQError
 
+VERSION = (0, 1)
+
+def get_version():
+    from zmq.sugar import pyzmq_version
+    return '%s.%s.%s' % (pyzmq_version(), VERSION[0], VERSION[1])
+
 from .client import Client
 from .server import Server
 from .publisher import Publisher
