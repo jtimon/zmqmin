34365c4cd3f35627f48b9e1668ff249c87d3f0b0
jtimon <jtimon@monetize.io>
2014-03-18 16:33:41 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+import os
+
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+requires = filter(lambda r:'libs/' not in r,
+    open(os.path.join(here, 'requirements.txt')).read().split())
+packages = filter(lambda p:not p.startswith('xunit'), find_packages())
+
+from zmqmin import get_version
+version = get_version()
+setup(**{
+    'name': 'zmqmin',
+    'version': version,
+    'description': u"Simplified python interface for ZMQ.",
+    'long_description': README,
+    'author':       'Jorge Timon',
+    'author_email': 'jtimon@monetize.io',
+    'url':          'http://www.github.com/jtimon/zmqmin/',
+    'download_url': 'http://pypi.python.org/packages/source/p/zmqmin/zmqmin-%s.tar.gz' % version.replace('.', '-'),
+    'packages': packages,
+    'classifiers': [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    'install_requires': requires,
+})
